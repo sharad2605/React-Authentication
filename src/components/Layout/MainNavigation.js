e8b22e21cd25fe4f639fb6425246d53b4a961bf4
sharad2605 <sharad26shukla@gmail.com>
2025-03-03 01:24:10 +0530

    Managing the token

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,8 +1,14 @@
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+
+import AuthContext from '../../Store/auth-context';
 
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
+
+  const authCtx = useContext(AuthContext);
+  const isLoggedIn = authCtx.isLoggedIn;
   return (
     <header className={classes.header}>
       <Link to='/'>
@@ -10,17 +16,25 @@ const MainNavigation = () => {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link to='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link to='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!isLoggedIn && (
+            <li>
+              <Link to='/auth'>Login</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <Link to='/profile'>Profile</Link>
+            </li>
+          )}
+          {isLoggedIn && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
+      
+     
     </header>
   );
 };
